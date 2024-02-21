862473fdd661e2f6f16fc6aadc373038fef9ac3b
E-vyatar <evyatarhadasi@gmail.com>
2024-02-21 10:17:11 +0100

    Add number of days choice
    - Add slider
    - Add label connected to the slider

--- src/main/java/org/example/shiftsprinter/ShiftsPrinterController.java
+++ src/main/java/org/example/shiftsprinter/ShiftsPrinterController.java
@@ -1,10 +1,7 @@
 package org.example.shiftsprinter;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.time.LocalDate;
 
@@ -16,17 +13,53 @@ public class ShiftsPrinterController {
     private Button printButton;
 
     @FXML
+    private Slider numDaysChooser;
+
+    @FXML
+    private Label sliderValue;
+
+    @FXML
     private TextField subjectTextField;
 
     @FXML
     private TextArea shiftsTextArea;
 
+    private final int SLIDER_MIN = 1;
+    private final int SLIDER_MAX = 30;
+    private final int SLIDER_VALUE = 7;
+
 
 
     @FXML
     private void initialize() {
-        // Initialization code
         datePicker.setValue(LocalDate.now());
+        sliderValue.setText(String.valueOf(numDaysChooser.getValue()));
+
+        // update label when slider value change
+        numDaysChooser.valueProperty().addListener((observable, oldValue, newValue) -> {
+            sliderValue.setText(String.valueOf(newValue.intValue()));
+        });
+
+        // update slider when label value change
+        sliderValue.setOnKeyTyped(event -> {
+            String eventText = event.getCharacter();
+            String labelValue = sliderValue.getText();
+            if (!eventText.matches("\\d")) {
+                event.consume();
+            } else {
+                labelValue += event.getCharacter();
+                int intValue = Integer.parseInt(labelValue);
+                if (intValue < 1 || intValue > 30) {
+                    event.consume();
+                } else {
+                    numDaysChooser.setValue(intValue);
+                }
+            }
+        });
+
+        numDaysChooser.setMin(SLIDER_MIN);
+        numDaysChooser.setMax(SLIDER_MAX);
+        numDaysChooser.setValue(SLIDER_VALUE);
     }
 
     @FXML
