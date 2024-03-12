aae6d069f7921c8a87897808fb1d1e9e419dd691
E-vyatar <evyatarhadasi@gmail.com>
2024-03-12 12:51:36 +0100

    Window look
    - Change window title
    - Change Window size to match content

--- src/main/java/org/example/shiftsprinter/ShiftsPrinterApplication.java
+++ src/main/java/org/example/shiftsprinter/ShiftsPrinterApplication.java
@@ -11,8 +11,8 @@ public class ShiftsPrinterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ShiftsPrinterApplication.class.getResource("UI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Shifts Printer");
         stage.setScene(scene);
         stage.show();
     }
